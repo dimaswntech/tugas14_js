71540dc22b53b752f21f0d604d2214c225e35839
dimaswntech <67653363+dimaswntech@users.noreply.github.com>
2021-12-21 16:08:37 +0700

    Add files via upload

---
+++ tugas14.js
@@ -0,0 +1,17 @@
+// 1. Pastikan Teman Teman Sudah Menginstall Node JS
+
+// 2. Teman - Teman Buatlah File di Folder javascript teman teman sebuah file dengan nama file tugas14.js
+
+// 3. Buatlah Program Dengan Spesifikasi dibawah ini :
+
+// Teman teman buatlah sebuah variabel untuk melakukan operasi pembagian , teman teman lakukan pembagian 37 dibagi tanggal lahir teman teman.
+// Setelah itu teman teman tampilkan hasil pembagian tersebut.
+// Setelah itu teman teman tampilkan kembali hasilnya dengan perintah math round , ceil dan floor.
+// Hint : -
+
+// 4. Happy Codding :)
+
+var x = 37/2;
+console.log(Math.ceil(x));
+console.log(Math.floor(x));
+console.log(Math.round(x));
